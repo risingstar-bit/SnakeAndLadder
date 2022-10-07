730ccc91e649ed4d2a553f017b6b7191710cf7a7
Muhammad Hamzah <20823685+mdhmz1@users.noreply.github.com>
2022-10-08 04:51:34 +0530

    Update GameConstants.java

--- src/main/java/common/GameConstants.java
+++ src/main/java/common/GameConstants.java
@@ -3,7 +3,7 @@ package main.java.common;
 public interface GameConstants {
 
   Integer NUMBER_OF_SNAKES = 10;
-  Integer DEFAULT_BOARD_SIZE = 150;
+  Integer DEFAULT_BOARD_SIZE = 100;
   Integer INITIAL_PLAYER_POSITION = 0;
   Integer GAME_RUN_COUNT = 100;
 
